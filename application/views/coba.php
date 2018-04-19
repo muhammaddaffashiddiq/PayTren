259f87ed02c2c5ad242b86d81ddb3b4be6188eab
muhammaddaffashiddiq <muhammaddaffa0708@gmail.com>
2018-04-20 00:44:36 +0700

    versi 6.0

---
+++ application/views/coba.php
@@ -0,0 +1,39 @@
+<?php <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Coba extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Load Dependencies
+
+	}
+
+	// List all your items
+	public function index( $offset = 0 )
+	{
+		
+	}
+
+	// Add a new item
+	public function add()
+	{
+
+	}
+
+	//Update one item
+	public function update( $id = NULL )
+	{
+
+	}
+
+	//Delete one item
+	public function delete( $id = NULL )
+	{
+
+	}
+}
+
+/* End of file coba.php */
+/* Location: ./application/views/coba.php */
