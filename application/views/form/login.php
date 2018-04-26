a3e78ddaa387bd4f594765e2b1abe81cee60eeff
muhammaddaffashiddiq <muhammaddaffa0708@gmail.com>
2018-04-26 17:09:42 +0700

    versi 6.0

---
+++ application/views/form/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="http://localhost/Tugas/index.php/User/login" method="post"">
+		<div class="header">
+		<input type="text" name="username"><br>
+		<input type="text" name="password"><br>
+		<input type="submit" name="" value="Login">
+	</div>
+	</form>
+	
+</body>
+</html>
