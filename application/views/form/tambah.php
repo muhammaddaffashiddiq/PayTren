21a9ebcca7372e4bcd4781880d4fc0e30a9071ed
muhammaddaffashiddiq <muhammaddaffa0708@gmail.com>
2018-05-08 08:56:01 +0700

    gg

--- application/views/form/tambah.php
+++ application/views/form/tambah.php
@@ -10,7 +10,7 @@
 		<center><input type="text" name="password" placeholder="password"></center><br>
 		<center><input type="text" name="fullname" placeholder="fullname"></center><br>
 		<center><input type="text" name="level" placeholder="level"></center><br>
-		<center><input type="submit" value="save"></center>
+		<center> <input type="submit" value="save"> </center>
 	</form>
 </body>
 </html>
